d98c8ac24856add2eba4689f980e408173c559d4
Rafael C. Viana <rchagas.sdi@gmail.com>
2021-04-22 12:16:47 -0300

    Funcionalidade de cadastrar usuário finalizada

--- src/gitflowsimulacao/Programa.java
+++ src/gitflowsimulacao/Programa.java
@@ -4,6 +4,7 @@ public class Programa {
 	public static void main(String[] args) {	
 		
 		System.out.println("Funcionalidade de cadastrar em andamento");
+		System.out.println("Funcionalidade de cadastrar finalizada!!");
 		
 	}
 }
